c6e63e3c46e90a8137662054ebeb1b04ea90644b
Mahesh Deshmukh <deshmukhmahesh009@gmail.com>
2021-10-10 23:15:09 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import './App.css';
-import {useState,useEffect} from 'react';
 import ShowList from './ShowList';
 import Summary from './Summary';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import useFetch from './useFetch';
 function App() {
   let baseUrl = "https://api.tvmaze.com/search/shows?q=all";
-  const [showData, setShowData] = useState(null);
-  const [isPending,setIsPending] = useState(true);
-  useEffect(() => {
-    fetch(baseUrl)
-      .then((response) => {
-        console.log(response.ok)
-        return response.json()})
-      .then((data) => {
-        //console.log(data);
-        setShowData(data);
-        setIsPending(false);
-      });
-  }, [baseUrl]);
+  const {data:showData, isPending, error} = useFetch(baseUrl);
+  
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path="/">
-            {isPending && <div>Loading...</div>}
+          {isPending && <div>Loading...</div>}
+          {error && <div>{ error }</div>}
           {showData && <ShowList showData={showData} />}
           </Route>
           <Route path="/summary/:id">
-          {showData && <Summary showData={showData}/>}
+          {showData && <Summary showData={showData} error={error} isPending={isPending}/>}
           </Route>
         </Switch>
       </Router>
--- src/Summary.js
+++ src/Summary.js
@@ -1,21 +1,34 @@
 import { useParams } from "react-router";
 import { useState,useEffect } from "react";
-const Summary = ( {showData}) => {
+import useFetch from "./useFetch";
+const Summary = ( ) => {
     const [summaryShow,setSummaryShow] = useState(null);
     const {id} = useParams();
-    
+    let baseUrl = "https://api.tvmaze.com/search/shows?q=all";
+    const {data:showData, isPending, error} = useFetch(baseUrl); 
+    let show = showData.filter(item=> item.show.id === Number(id)); 
+    setSummaryShow(show);
+//   useEffect(()=>{
+   
+   
+      
+//   })
+      
+   
     //console.log(id);
-   useEffect(()=>{
-      let show = showData.filter(item=> item.show.id === Number(id))
-      setSummaryShow(show);
-     // console.log(summaryShow)
-   },[showData, id])
+   // useEffect(()=>{
+   //    let show = showData.filter(item=> item.show.id === Number(id))
+   //    setSummaryShow(show);
+   //   console.log(error)
+   // },[showData, id,error,isPending])
    
 
         
     return ( 
         <div className="summary">
            <h2>summary</h2>
+           {/* {isPending && <div>Loading...</div>}
+           {error && <div><p>no...</p></div>} */}
            {summaryShow && <div>{summaryShow[0].show.name}</div>}
            {summaryShow && <div>{summaryShow[0].show.summary}</div>}
         </div>  
